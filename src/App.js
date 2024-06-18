059ebc0de1180e0aa9533f5eec5d19c453b5d28d
Bruno195 <brunomonteiro195@gmail.com>
2024-06-18 02:59:42 -0300

    feat: first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [rick, setRick] = useState({})
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const fetchQuote =  async () => {
+      try {
+        const response = await axios.get('https://rickandmortyapi.com/api/character/?page=1')
+
+        setRick(response.data.results[0])
+        setLoading(false)
+      } catch (error){
+        setError('Erro ao buscar rick', error)
+        setLoading(false)
+      }
+    }
+
+    fetchQuote()
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='App'>
+    <header className='App-header'>
+    {
+      loading ? (
+        <p>Carregando ...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+
+        <>
+      <p>Name: {rick.name}</p>
+      <p>Status: {rick.status} </p>
+      {rick.image && <img src={rick.image} alt={rick.name}></img>}        
+      </>
+      )      
+    }
+
+     
+    </header>
+   </div>
   );
 }
 
